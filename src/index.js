37a333bee3c7a145e100f07d38e50a5a36165ff2
Wenxin Li <likhoram24@gmail.com>
2025-12-04 11:51:23 -0600

    wave 3 and 6

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ const cityInput = document.getElementById("cityNameInput")
 cityInput.addEventListener("input", () => {
     cityName.textContent = cityInput.value;
 });
+
+//wave 6
+const defaultCity = "Seattle";
+const resetButton = document.getElementById("cityNameReset")
+
+resetButton.addEventListener("click", () => {
+    cityInput.value = defaultCity;
+    cityName.textContent = defaultCity;
+});
